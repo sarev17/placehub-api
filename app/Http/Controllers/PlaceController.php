a69b826778f93ade418c61a6308e4d468ff16141
andre.av <andre.av@pf.gov.br>
2024-07-31 13:32:10 -0300

    feat: create PlaceController

---
+++ app/Http/Controllers/PlaceController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PlacesRequest;
+use App\Http\Services\API\ApiResponseService;
+use App\Models\Place;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PlaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PlacesRequest $request): JsonResponse
+    {
+        try {
+            $place = Place::create($request->all());
+            return ApiResponseService::success('created!', $place->only('id','name','slug','city','state'), 201);
+        } catch (Exception $e) {
+            return ApiResponseService::error('Failed to create place', 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
